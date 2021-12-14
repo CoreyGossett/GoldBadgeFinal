78bfe1ed6c2b9e3db7250814014989cda68120ff
Corey Gossett <coreymgossett@gmail.com>
2021-12-14 18:29:50 -0500

    Started Repo Methods

---
+++ Challenge2/Challenge2.Repo/ClaimRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2.Repo
+{
+    class ClaimRepo
+    {
+        public Queue<Claim> _claimQueue = new Queue<Claim>();
+
+        public bool CreateClaim(Claim claim)
+        {
+            if (claim is null)
+            {
+                return false;
+            }
+            _claimQueue.Enqueue(claim);
+            return true;
+        }
+
+        public Queue<Claim> GetClaims()
+        {
+            return _claimQueue;
+        }
+
+        public bool GetNextClaim()
+        {
+            _claimQueue.Dequeue();
+            return true;
+        }
+    }
+}
