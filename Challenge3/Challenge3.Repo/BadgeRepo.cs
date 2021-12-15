f3ade02b63bbde8f1457d656f4a438c8abcab78c
Corey Gossett <coreymgossett@gmail.com>
2021-12-15 18:34:23 -0500

    AddBadge Method

---
+++ Challenge3/Challenge3.Repo/BadgeRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge3.Repo
+{
+    public class BadgeRepo
+    {
+        Dictionary<int, List<string>> _badgeRepo = new Dictionary<int, List<string>>();
+
+        private int _count = 0;
+
+        public bool AddBadge(Badge badge)
+        {
+            if (badge is null)
+            {
+                return false;
+            }
+            _count++;
+            badge.BadgeID = _count;
+            _badgeRepo.Add(badge.BadgeID, badge.DoorNames);
+            return true;
+        }
+    }
+}
