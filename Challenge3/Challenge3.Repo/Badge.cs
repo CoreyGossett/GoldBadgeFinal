844a6b6978e69a5ddd149272a901d659d8f46f83
Corey Gossett <coreymgossett@gmail.com>
2021-12-15 18:19:23 -0500

    Badge Class

---
+++ Challenge3/Challenge3.Repo/Badge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge3.Repo
+{
+    public class Badge
+    {
+        public int BadgeID { get; set; }
+
+        public List<string> DoorNames { get; set; }
+
+        public string Name { get; set; }
+
+        public Badge()
+        {
+
+        }
+
+        public Badge(int badgeID, List<string> doorNames, string name)
+        {
+            BadgeID = badgeID;
+            DoorNames = doorNames;
+            Name = name;
+        }
+    }
+}
